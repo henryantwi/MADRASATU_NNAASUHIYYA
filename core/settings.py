11f8fc9c2123a5e627ae20358e3b785c184c822e
henryantwi <henryantwi191@gmail.com>
2024-09-22 18:29:37 +0000

    troubleshoting password rest 001

--- core/settings.py
+++ core/settings.py
@@ -148,16 +148,26 @@ ACCOUNT_USERNAME_BLACKLIST = [
     "dues",
 ]
 
-if ENVIRONMENT == 'production' or POSTGRES_LOCALLY is True:
-    EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
-    EMAIL_HOST = env("EMAIL_HOST", default="smtp.gmail.com")
-    EMAIL_PORT = env("EMAIL_PORT", default=465)
-    EMAIL_USE_TLS = env("EMAIL_USE_TLS", default=False)
-    EMAIL_USE_SSL = env("EMAIL_USE_SSL", default=True)
-    EMAIL_HOST_USER = env("EMAIL_HOST_USER")
-    EMAIL_HOST_PASSWORD = env("EMAIL_HOST_PASSWORD")
-    DEFAULT_FROM_EMAIL = f"MadrasatuPay {EMAIL_HOST_USER}"
-    ACCOUNT_EMAIL_SUBJECT_PREFIX = ""
-else:
-    EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+# if ENVIRONMENT == 'production' or POSTGRES_LOCALLY is True:
+#     EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
+#     EMAIL_HOST = env("EMAIL_HOST", default="smtp.gmail.com")
+#     EMAIL_PORT = env("EMAIL_PORT", default=465)
+#     EMAIL_USE_TLS = env("EMAIL_USE_TLS", default=False)
+#     EMAIL_USE_SSL = env("EMAIL_USE_SSL", default=True)
+#     EMAIL_HOST_USER = env("EMAIL_HOST_USER")
+#     EMAIL_HOST_PASSWORD = env("EMAIL_HOST_PASSWORD")
+#     DEFAULT_FROM_EMAIL = f"MadrasatuPay {EMAIL_HOST_USER}"
+#     ACCOUNT_EMAIL_SUBJECT_PREFIX = ""
+# else:
+#     EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 
+
+EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
+EMAIL_HOST = env("EMAIL_HOST", default="smtp.gmail.com")
+EMAIL_PORT = env("EMAIL_PORT", default=465)
+EMAIL_USE_TLS = env("EMAIL_USE_TLS", default=False)
+EMAIL_USE_SSL = env("EMAIL_USE_SSL", default=True)
+EMAIL_HOST_USER = env("EMAIL_HOST_USER")
+EMAIL_HOST_PASSWORD = env("EMAIL_HOST_PASSWORD")
+DEFAULT_FROM_EMAIL = f"MadrasatuPay {EMAIL_HOST_USER}"
+ACCOUNT_EMAIL_SUBJECT_PREFIX = ""
