d48700e1bee2288c31c6d62de082554f272fb7e3
henryantwi <henryantwi191@gmail.com>
2024-09-23 03:40:43 +0000

    Refactor settings.py and remove unused middleware and template

--- core/settings.py
+++ core/settings.py
@@ -17,14 +17,15 @@ if ENVIRONMENT == "development":
     DEBUG = True
 elif ENVIRONMENT == "production":
     DEBUG = False
-# DEBUG = True
 
 ALLOWED_HOSTS = ["*"]
 
 # Application definition
 
 INSTALLED_APPS = [
-    # "unfold",
+    # Admin Panel Styling
+    "unfold",
+    
     # Default apps:
     "django.contrib.humanize",
     "django.contrib.admin",
@@ -52,7 +53,6 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
-    # "middleware.LogRequestMiddleware",
 ]
 
 ROOT_URLCONF = "core.urls"
