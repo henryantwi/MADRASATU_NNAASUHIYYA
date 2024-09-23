e1e69329807c084878e4fb8427d52c2926291ff0
henryantwi <henryantwi191@gmail.com>
2024-09-23 03:42:44 +0000

    Refactor password reset views and URLs

--- account/urls.py
+++ account/urls.py
@@ -1,4 +1,5 @@
 from django.urls import path
+
 from . import views
 
 app_name = "account"
@@ -6,29 +7,12 @@ app_name = "account"
 urlpatterns = [
     # Authentication
     path("login/", views.login_view, name="login"),
-    path('logout/', views.sign_out, name='logout'),
-
+    path("logout/", views.sign_out, name="logout"),
     path("register/", views.register_view, name="register"),
-    path("activate/<slug:uidb64>/<slug:token>/", views.account_activate, name="activate"),
-
+    path(
+        "activate/<slug:uidb64>/<slug:token>/", views.account_activate, name="activate"
+    ),
     # User Profile
     path("profile/", views.profile_view, name="profile"),
     path("faq/", views.faq_view, name="faq"),
-
-    # Password reset
-    path('password_reset/', views.CustomPasswordResetView.as_view(), name='password_reset'),
-    path('password_reset/done/', views.CustomPasswordResetDoneView.as_view(), name='password_reset_done'),
-    path('reset/<uidb64>/<token>/', views.CustomPasswordResetConfirmView.as_view(), name='password_reset_confirm'),
-    path('reset/done/', views.CustomPasswordResetCompleteView.as_view(), name='password_reset_complete'),
-    
-    path(
-        "password_reset/",
-        auth_views.PasswordResetView.as_view(
-            template_name="account/password_reset/password_reset_form.html",
-            success_url="password_reset_email_confirm",
-            email_template_name="account/password_reset/password_reset_email.html",
-            form_class=PwdResetForm,
-        ),
-        name="pwdreset",
-    ),
 ]
